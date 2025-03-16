08ce05f285f5ba95fa62f36a4de0e8595aa3c90a
Siddhant-Dixit22 <dixit.siddhant22@gmail.com>
2025-03-16 19:12:58 -0400

    Setup
    Made landing page and auth pages, nothing implemented yet.

---
+++ planner/src/components/navbar.tsx
@@ -0,0 +1,47 @@
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuList,
+} from "@/components/ui/navigation-menu"
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const NavBar = () => {
+    return (
+        <div className="border-b w-full">
+            <nav className="container mx-auto p-4">
+                <div className="flex justify-between items-center">
+                    <div className="flex-shrink-0">
+                        <Link href="/">
+                            <Image src="globe.svg" alt="logo" width={60} height={10}/>
+                        </Link>
+                    </div>
+                    
+                    <div className="flex items-center space-x-10">
+                        <Button variant="ghost">
+                            <Link href={"/sign-up"}>Dashboard</Link>
+                        </Button>
+                        <Button variant="ghost">
+                            <Link href={"/sign-up"}>Periods</Link>
+                        </Button>
+                        <Button variant="ghost">
+                            <Link href={"/sign-up"}>Classes</Link>
+                        </Button>
+                        <Button variant="ghost">
+                            <Link href={"/sign-up"}>Tasks</Link>
+                        </Button>
+                    </div>
+
+                    <div className="flex-shrink-0">
+                        <Button variant="default">
+                            <Link href={"/sign-up"}>Register Now</Link>
+                        </Button>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default NavBar;
