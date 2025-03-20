908fbf194801466b9880a0d444c296c26e6ebfed
Siddhant-Dixit22 <dixit.siddhant22@gmail.com>
2025-03-20 17:04:14 -0400

    Set up appwrite
    Set up the connection to appwrite with hono cookies.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Planner Website
  Website for tracking school work, office work, etc
+
+#Deployment Website
+[Website:](https://planner-website-livid.vercel.app/)
--- planner/src/features/auth/server/route.ts
+++ planner/src/features/auth/server/route.ts
@@ -2,6 +2,10 @@ import { z } from "zod";
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { signinSchema } from "../schemas";
+import { createAdminClient } from "@/lib/appwrite";
+import { ID } from "node-appwrite";
+import { deleteCookie, setCookie } from "hono/cookie";
+import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
     .post(
@@ -9,8 +13,23 @@ const app = new Hono()
         zValidator("json", signinSchema), 
         async (c) => {
             const { email, password } = c.req.valid("json");
+
+            const { account } = await createAdminClient();
+            const session = await account.createEmailPasswordSession(
+                email,
+                password
+            );
+
+            setCookie(c, AUTH_COOKIE, session.secret, {
+                path: "/",
+                httpOnly: true,
+                secure: true,
+                sameSite: "strict",
+                maxAge: 60 * 60 * 24 * 7
+            });
+
             console.log( email, password );
-            return c.json({ success: "ok"});
+            return c.json({ success: true });
         }
     )
     .post(
@@ -22,9 +41,38 @@ const app = new Hono()
         })), 
         async (c) => {
             const { name, email, password } = c.req.valid("json");
-            console.log( name, email, password );
-            return c.json({ success: "ok"});
+            
+            const { account } = await createAdminClient();
+            await account.create(
+                ID.unique(),
+                email,
+                password,
+                name
+            );
+
+            const session = await account.createEmailPasswordSession(
+                email,
+                password
+            );
+
+            setCookie(c, AUTH_COOKIE, session.secret, {
+                path: "/",
+                httpOnly: true,
+                secure: true,
+                sameSite: "strict",
+                maxAge: 60 * 60 * 24 * 7
+            });
+
+            return c.json({ success: true });
         }
-    );
+    )
+    .post(
+        "/signout",
+        (c) => {
+            deleteCookie(c, AUTH_COOKIE);
+
+            return c.json({ success: true });
+        }
+    )
 
 export default app;
